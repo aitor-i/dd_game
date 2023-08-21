940ef704cb7060ba6556a8bcfb9b9f460d514ddf
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-08-21 17:25:00 +0200

    ADD - Training fight

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ use std::io;
 mod domain;
 mod service;
 
+use crate::domain::char_enum;
 use crate::domain::characters::Character;
+use crate::domain::goblin::Goblin;
 use crate::service::messages::print_title;
 use crate::service::messages::print_options;
 use crate::service::set_your_character::set_your_character;
+use crate::domain::base_character::CharacterActions;
 
 fn main() {
     print_title();
@@ -38,7 +41,7 @@ fn main() {
 
         match option { 
             1 => {  character = set_your_character()},
-            2 => println!("Todo"),
+            2 => { training_fight() },
             9 => { 
                 println!("Quiting ...");
                 continue;
@@ -49,5 +52,49 @@ fn main() {
         println!();
     };
 
+    fn training_fight() -> () { 
+        println!("A goblin apear on yor way, it seems to be dangerous");
+        println!("What dou you wanna do?");
+        
+        let mut goblin = Goblin::new(1);
+        let mut goblin2 = Goblin::new(1);
+
+
+        loop { 
+            fight_option();
+
+            let mut option = String::new();
+
+            io::stdin()
+                .read_line(&mut option)
+                .expect("Failed to read option!");
+
+            let option: u8 = match option.trim().parse() { 
+                Ok(num)=> num,
+                Err(_) => { 
+                    println!("{} is not a valid option", option);
+                    continue;
+                }
+            };
+
+            match option { 
+                1 => {calculate_attack(& mut goblin2, &  mut goblin);}
+                2 => println!("Todo"),
+                _ => println!("{} is not a valid option", {option})
+            }
+        }   
+    }
+
+    fn calculate_attack(attacker: &mut dyn CharacterActions , defender: &mut dyn CharacterActions) -> () { 
+        println!("Attacking...");
+        println!("Initial heath: {}", defender.get_health());
+        defender.recieve_damage( attacker.attack() );
+        
+    }
+
+    fn fight_option() -> () { 
+        println!("1 - Attack");
+        println!("2 - Defend");
+    }
 
 }
