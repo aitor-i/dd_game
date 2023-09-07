06d053d91c29b9e0ea75168952a5bb0e11d8f13e
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-09-07 10:26:06 +0200

    UPDATE(inventary) - add_to_inventary

--- src/domain/inventary.rs
+++ src/domain/inventary.rs
@@ -1,6 +1,5 @@
 pub struct Inventary {
     pub  items:Vec<InventaryItem>
-
 }
 
 impl Inventary { 
@@ -11,9 +10,24 @@ impl Inventary {
         }
     }
     
-   pub fn add_to_inventary(&mut self, item: InventaryItem)-> () {
-        println!("Item added to inventary!" );
-        self.items.push(item); 
+    pub fn add_to_inventary(&mut self, item: InventaryItem)-> () {
+        if self.items.len() == 0 { 
+            self.items.push(item);
+
+        }else { 
+            for item_from_inventary in &mut self.items.clone() { 
+                let item_clone = item.clone();
+                if item_from_inventary.name == item.name && item_from_inventary.power == item.power && item_from_inventary.description == item.description && item_from_inventary.effect_time == item.effect_time { 
+
+                    item_from_inventary.times = item_from_inventary.times + 1;
+                }else { 
+                    self.items.push(item_clone); 
+                } 
+
+            }
+
+            println!("Item added to inventary!" );
+        }
     }
 
    pub fn use_item(&mut self, item_name:InventaryItemsEnum)-> () { 
@@ -26,10 +40,10 @@ impl Inventary {
         let filterd_items : Vec<InventaryItem> = self.items.iter().filter(|&ref item| item.times > 0).cloned().collect();
         println!("Filterd items {:#?}", filterd_items);
         self.items = filterd_items;
-        
     }
     
 }
+
 #[derive(Debug)]
 #[derive(Clone)]
 pub struct InventaryItem { 
