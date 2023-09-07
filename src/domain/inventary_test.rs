ffe23e4fd6208abc08f97a4c081ebbcb600790e5
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-09-07 10:25:25 +0200

    TEST - should_not_be_duplicate_items

--- src/domain/inventary_test.rs
+++ src/domain/inventary_test.rs
@@ -9,7 +9,7 @@ mod tests {
         let power: u8 = 1;
         let effect_time: u8 = 1;
         let description = String::from("This potion heals!");
-        let mut item = InventaryItem::new(InventaryItemsEnum::HealingPotion, description, power, effect_time);
+        let item = InventaryItem::new(InventaryItemsEnum::HealingPotion, description, power, effect_time);
 
         inventary.add_to_inventary(item);
 
@@ -19,17 +19,31 @@ mod tests {
     
     #[test]
     fn used_element_should_pop_from_inventary(){ 
-
         let mut inventary: Inventary = Inventary::new();
         let power: u8 = 1;
         let effect_time: u8 = 1;
         let description = String::from("This potion heals!");
-        let mut item = InventaryItem::new(InventaryItemsEnum::HealingPotion, description, power, effect_time);
+        let item = InventaryItem::new(InventaryItemsEnum::HealingPotion, description, power, effect_time);
 
         inventary.add_to_inventary(item);
         inventary.use_item(InventaryItemsEnum::HealingPotion);
 
         assert_eq!(inventary.items.len(), 0);
+    }
+
+    #[test]
+    fn should_not_be_duplicate_items(){ 
+        let mut inventary: Inventary = Inventary::new();
+        let power: u8 = 1;
+        let effect_time: u8 = 1;
+        let description = String::from("This potion heals!");
+        let item = InventaryItem::new(InventaryItemsEnum::HealingPotion, description, power, effect_time);
+
+        inventary.add_to_inventary(item.clone());
+        inventary.add_to_inventary(item);
+
+        assert_eq!(inventary.items.len() , 1);
+        
     }
 
 }
