1ceffadcf39b5541edf1a843c6da88fb6f1e6510
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-09-10 16:22:50 +0200

    TEST - Character should recieve deamage

--- src/domain/characters_test.rs
+++ src/domain/characters_test.rs
@@ -1,12 +1,12 @@
 #[cfg(test)]
 
 mod tests{ 
-use crate::domain::base_character::CharacterActions;
-use crate::domain::characters::CharacterBuild;
-use crate::domain::characters::Character;
+    use crate::domain::base_character::CharacterActions;
+    use crate::domain::characters::CharacterBuild;
+    use crate::domain::characters::Character;
 
-#[test]
-fn should_create_a_charcter_with_paladin_build_stats() { 
+    #[test]
+    fn should_create_a_charcter_with_paladin_build_stats() { 
         let name_test = String::from("John"); 
         let paladin_build_stats = Character { 
             name: name_test.clone(),
@@ -19,5 +19,25 @@ fn should_create_a_charcter_with_paladin_build_stats() {
         assert_eq!(paladin.get_health(), paladin_build_stats.health, "Paladin stats sould match" );
         assert_eq!(paladin.get_agility(), paladin_build_stats.agiliti, "Paladin stats sould match" );
     }
-    
+
+    #[test]
+    fn characters_should_recieve_damage() { 
+        let name_test = String::from("John"); 
+        let mut paladin_mock = Character { 
+            name: name_test.clone(),
+            health : 54,
+            attack_power : 6, 
+            agiliti :0, // This way cant never evade an attack; 
+        };
+        let damage:u8 = 5;
+        let expected_remaining_health:u8 = 49;
+
+        paladin_mock.recieve_damage(damage);
+
+        assert_eq!(paladin_mock.get_health(), expected_remaining_health, "Character should recieve 5 points of damage");
+        
+    }
+
+
+
 }
