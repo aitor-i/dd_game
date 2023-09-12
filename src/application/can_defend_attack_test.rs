48005d324f3c9fb72c14ff8b00f2ff43d86b1219
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-09-12 07:36:11 +0200

    TEST - should_not_defend_attack

--- src/application/can_defend_attack_test.rs
+++ src/application/can_defend_attack_test.rs
@@ -21,5 +21,22 @@ use crate::application::can_defend_attack::can_defend_attack;
         assert_eq!(is_defended, true);
 
     }
+    #[test]
+    fn should_not_deffend_attack() { 
+        
+        let name_test = String::from("John"); 
+        //let mut paladin_mock = Character::new(CharacterBuild::Paladin, name_test.clone());
+        let mut paladin_mock = Character { 
+            name: name_test.clone(),
+            health : 50,
+            attack_power : 6, 
+            agiliti :0, // This way can evade an attack; 
+        } ;
 
+
+        let is_defended = can_defend_attack(& mut paladin_mock);
+
+        assert_eq!(is_defended, false);
+
+    }
 }
